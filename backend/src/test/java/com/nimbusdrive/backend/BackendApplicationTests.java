42900e85a86f281c59db521a7bcbecf9a3d60735
Alex potter <alexpandiyan1914@gmail.com>
2026-06-06 22:58:45 +0530

    Initialize Nimbus Drive Project Structure

---
+++ backend/src/test/java/com/nimbusdrive/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nimbusdrive.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
